28ff70c700fd2340964c4db51ffe3338faa7ea24
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-12-13 14:01:06 +0300

    refactor: deleted redundant constructors

--- src/main/java/org/siriys/exceptions/BasketOverflowException.java
+++ src/main/java/org/siriys/exceptions/BasketOverflowException.java
@@ -1,23 +1,7 @@
 package org.siriys.exceptions;
 
 public class BasketOverflowException extends RuntimeException {
-    public BasketOverflowException() {
-        super();
-    }
-
     public BasketOverflowException(String message) {
         super(message);
-    }
-
-    public BasketOverflowException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public BasketOverflowException(Throwable cause) {
-        super(cause);
-    }
-
-    protected BasketOverflowException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/org/siriys/exceptions/CantMakeBedException.java
+++ src/main/java/org/siriys/exceptions/CantMakeBedException.java
@@ -1,23 +1,7 @@
 package org.siriys.exceptions;
 
 public class CantMakeBedException extends Exception {
-    public CantMakeBedException() {
-        super();
-    }
-
     public CantMakeBedException(String message) {
         super(message);
-    }
-
-    public CantMakeBedException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public CantMakeBedException(Throwable cause) {
-        super(cause);
-    }
-
-    protected CantMakeBedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
